988bb86c6d765e07fde335f3cf4f25c6d5e53e84
Douglas Wendel <dpwendel@email.sc.edu>
2021-11-09 15:36:00 -0500

    added login and create account page

--- App.js
+++ App.js
@@ -1,58 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ImageBackground, StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Button, Image, Alert, SafeAreaView, TextInput, ImageBackground } from 'react-native';
 
+const image = { uri: ""}
+const Stack = createNativeStackNavigator();
+const Seperator = () => (
+  <View style = {styles.seperator} />
+);
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('./assets/GrubberBackground.png')} resizeMode='cover' style={styles.backgroundImage}>
-      <Text style={styles.text}>Welcome to Grubber!</Text>
-      <Image source={require('./assets/grubber.png')} style={styles.logo}/>
-      <Button
-        title="Click me"
-        color="orange"
-        style={styles.loginButton}
-        onPress={() =>
-          Alert.alert("Title", "Message", [
-            {text: "Yes", onPress: () => console.log("Yes") },
-            {text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-      <Button
-        title="Log in"
-        color= "blue"
-        style={styles.loginButton}
-        onPress={() =>
-          Alert.alert("Title", "Message", [
-            {text: "Yes", onPress: () => console.log("Yes") },
-            {text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-      <Button
-        title="Create an account"
-        color= "blue"
-        style={styles.loginButton}
-        onPress={() =>
-          Alert.alert("Would you like to create an account", [
-            {text: "Yes", onPress: () => console.log("Yes") },
-            {text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer> 
+      <Stack.Navigator>
+        <Stack.Screen
+        name="Home"
+        component = {HomeScreen}
+        options = {{ title: 'Grubber'}}
+        />
+        <Stack.Screen name = "Login" component={LoginScreen} />
+        <Stack.Screen name = "CreateAnAccount" component={NewAccountScreen} />
+        <Stack.Screen name = "Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+const HomeScreen = ({ navigation }) => {
+  return (
+  <SafeAreaView style={styles.container}>
+    <ImageBackground
+      source={ require('./assets/GrubberBackground.png')}
+      resizeMode='cover' style={styles.backgroundImage}/>
+      <Text> Welcome to Grubber! </Text>
+      <View>
+    <Button
+    title="Log in"
+    color="blue"
+    onPress={() =>
+    navigation.navigate('Login')
+  } />
+  </View>
+  <Seperator/>
+    <View>
+      <Text style={styles.title}>
+        </Text>
+        <Button
+        title="Create an account"
+        color="blue"
+        onPress={() =>
+        navigation.navigate('CreateAnAccount')
+    } />
+    </View>
+  <Seperator/>
+  <View>
+      <Text style={styles.title}>
+        </Text>
+        <Button
+        title="Settings"
+        color="blue"
+        onPress={() =>
+        navigation.navigate('Settings')
+    } />
+    </View>
+    </SafeAreaView>
+  );
+};
+const LoginScreen = ({ navigation, route }) => { 
+  return (
+          <Text> Please enter your email and password.</Text>
+  );
+};
+const NewAccountScreen = ({ navigation, route }) => { 
+  return(
+
+   <Text> Name: </Text>
+  );
+};
+const Settings = ({ navigation, route }) => { 
+  return ( <Text> Welcome to Settings </Text>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
   logo: {
     position: 'absolute',
@@ -77,7 +126,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  loginButton: {
-    margin: 5,
-  },
 });
+
+export default App;
