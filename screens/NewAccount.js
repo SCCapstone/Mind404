0a05e3ffcdff47a12a4377463a03ee162645e09f
Douglas Wendel <dpwendel@email.sc.edu>
2021-11-15 12:46:48 -0500

    finished create account page

--- screens/NewAccount.js
+++ screens/NewAccount.js
@@ -1,24 +1,75 @@
-import * as React from 'react';
-import { Text, StyleSheet, ImageBackground } from 'react-native'
-import Button from "../components/Button.js";
+import * as React from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+import Button from "../components/Button";
 
 const NewAccountScreen = ({ navigation, route }) => { 
+    const [text, onChangeText] = React.useState("text");
+    const [number, onChangeNumber] = React.useState(null);
     return(
     <ImageBackground
-        source={ require('../assets/GrubberBackground.png')}
-        resizeMode='cover' style={styles.backgroundImage}>
-            <Text> New Account page </Text>
-            <Text> WILL BE BUNCH OF INPUT HERE </Text>
-            <Button onPress={() => navigation.navigate("Map")}>Login</Button>
+      source={require("../assets/GrubberBackground.png")}
+      resizeMode="cover"
+      style={styles.backgroundImage}
+    >
+      <Text style={styles.BiggerText}>
+        {" "}
+        Please enter your information below.
+      </Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="First Name"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="Last Name"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="Date of Birth"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="email"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="password"
+        />
+        <Button onPress={() => navigation.navigate("Map")}>Login</Button>
+      </SafeAreaView>
     </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-      width: '100%',
-      height: '100%',
+    BiggerText: {
+      fontSize: 20,
+      textAlign: "center",
     },
-});
+    backgroundImage: {
+      width: "100%",
+      height: "100%",
+    },
+    input: {
+      height: -200,
+      margin: 10,
+      borderWidth: 4,
+      padding: 20,
+      fontSize: 20,
+    },
+  });
 
 export default NewAccountScreen
