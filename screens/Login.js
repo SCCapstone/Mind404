e0ae51f1b54d0c492d428b1c87fb619f2a02d28b
Adam Farley <adamfarley4@gmail.com>
2021-11-14 19:43:09 -0500

    added screens, added input to login

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,48 @@
 import * as React from 'react';
-import { Text, StyleSheet, ImageBackground } from 'react-native'
+import { SafeAreaView, Text, StyleSheet, ImageBackground, TextInput } from 'react-native'
 
 const LoginScreen = ({ navigation, route }) => { 
+  const[text,onChangeText] = React.useState("text");
+  const[number,onChangeNumber] = React.useState(null);
     return (
     <ImageBackground
-    source={ require('../assets/GrubberBackground.png')}
-    resizeMode='cover' style={styles.backgroundImage}>
-            <Text> Please enter your email and password.</Text>
+      source={ require('../assets/GrubberBackground.png')}
+      resizeMode='cover' style={styles.backgroundImage}>
+      <Text style = {styles.BiggerText}> Please enter your email and password.</Text>
+
+      <SafeAreaView>
+        <TextInput
+        style = {styles.input}
+        onChangeText = {onChangeText}
+        placeholder="email"
+      />
+      <TextInput
+      style = {styles.input}
+      onChangeText = {onChangeNumber}
+      value = {number}
+      placeholder="password"
+      />
+    </SafeAreaView>
     </ImageBackground>
+
+
     );
 };
 
 const styles = StyleSheet.create({
+  BiggerText:{
+    fontSize: 20,
+  },
     backgroundImage: {
       width: '100%',
       height: '100%',
     },
+    input: {
+      height: -200,
+      margin: 10,
+      borderWidth: 4,
+      padding: 20,
+    }
 });
 
 export default LoginScreen
