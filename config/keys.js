ffe67074882c3e68723fc52a5c1d99092e04102f
tylershatley <shatleymtyler@gmail.com>
2021-11-17 17:56:50 -0500

    firebase stuff

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyCS9619pnoIPyrZOd71wleW0yLb7YqKjII",
+        authDomain: "grubber-4a87a.firebaseapp.com",
+        databaseURL: "https://grubber-4a87a-default-rtdb.firebaseio.com",
+        projectId: "grubber-4a87a",
+        storageBucket: "grubber-4a87a.appspot.com",
+        messagingSenderId: "451766757997",
+        appId: "1:451766757997:web:060d4f1ee34a492ab56d64",
+        measurementId: "G-FHEB3MJM04"
+      }
+}
