329caacdea5aeb1dd8e71b3f575461fda646028e
tylershatley <shatleymtyler@gmail.com>
2021-11-30 19:34:39 -0500

    satisfy valid user registration

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -4,21 +4,71 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import styles from './../../../components/styles';
 import { firebase } from '../../firebase/config'
 
+
 export default function RegistrationScreen({navigation}) {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [dob, setDOB] = useState('')
     const [password, setPassword] = useState('')
+    const [typeOfUser, setTypeOfUser] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    var customer = "CUSTOMER"
+    var client = "CLIENT"
 
     const onFooterLinkPress = () => {
         navigation.navigate('Login')
     }
 
     const onRegisterPress = () => {
-        if (password !== confirmPassword) {
+
+        /** checks to see if passwords match */
+        if (password !== confirmPassword){
             alert("Passwords don't match.")
+            return
+        }
+
+        /**checks to see if valid type of user is entered */
+        if (typeOfUser.toUpperCase() !== customer){
+            if (typeOfUser.toUpperCase() !== client) {
+                alert("Invalid Type of User entry")
+                return
+            }
+        }
+
+        /** checks to see if a first name is entered into the field */
+        if (firstName == ''){
+            alert("First Name field cannot be empty")
+            return
+        }
+
+        /** checks to see if a last name is entered into the field */
+        if (lastName == ''){
+            alert("Last Name field cannot be empty")
+            return
+        }
+
+        /** checks to see if email field is empty */
+        if (email == ''){
+            alert("Email field cannot be empty")
+            return
+        }
+
+        /** checks to see if valid date of birth entry is present */
+        if(!isValidDate(dob)){
+            alert("Invalid date of birth entry")
+            return
+        }
+
+        /**checks to see if password field is empty */
+        if(password == ''){
+            alert("Password field cannot be empty")
+            return
+        }
+
+        /**checks to see if confirm password field is empty */
+        if(confirmPassword == ''){
+            alert("Confirm password field cannot be empty")
             return
         }
         firebase
@@ -31,7 +81,8 @@ export default function RegistrationScreen({navigation}) {
                 email,
                 firstName,
                 lastName,
-                dob
+                dob,
+                typeOfUser
             };
             const usersRef = firebase.firestore().collection('users')
             usersRef
@@ -90,10 +141,19 @@ export default function RegistrationScreen({navigation}) {
                 />
                 <TextInput
                     style={styles.input}
-                    placeholder='Date of Birth'
+                    placeholder='Date of Birth (mm/dd/yyy)'
                     placeholderTextColor="#aaaaaa"
                     onChangeText={(text) => setDOB(text)}
                     value={dob}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Type of User (customer or client)'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setTypeOfUser(text)}
+                    value={typeOfUser}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
@@ -129,3 +189,30 @@ export default function RegistrationScreen({navigation}) {
         </ImageBackground>
     )
 }
+
+/** algorithm to check to see if a valid date is present within a string*/
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[1], 10);
+    var month = parseInt(parts[0], 10);
+    var year = parseInt(parts[2], 10);
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
