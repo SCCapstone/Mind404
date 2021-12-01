8cd302f7f7362739566a0252be57adf1c1250d20
Douglas Wendel <dpwendel@email.sc.edu>
2021-12-01 18:35:29 -0500

    user profile prints name from firebase

--- src/screens/clientScreens/ProfileScreen/ProfileScreen.js
+++ src/screens/clientScreens/ProfileScreen/ProfileScreen.js
@@ -1,8 +1,15 @@
 import * as React from "react";
 import { Text, View, ImageBackground } from "react-native";
 import styles from "./../../../../components/styles";
+import { firebase } from "../../../firebase/config";
+
 
 export default function ProfileScreen() {
+  const user = firebase.auth().currentUser;
+  user.providerData.forEach((userInfo) => {
+    console.log('User info for provider: ', userInfo);
+  })
+
   return (
     <ImageBackground
       source={require("../../../../assets/GrubberBackground.png")}
@@ -10,8 +17,8 @@ export default function ProfileScreen() {
       style={styles.backgroundImage}
     >
       <View>
-        <Text>Profile</Text>
+        <Text style ={styles.title}> Welcome {user.userInfo}</Text>
       </View>
-    </ImageBackground>
+    </ImageBackground>    
   );
 }
--- src/screens/providerScreens/ProvServicesScreen/ProvServicesScreen.js
+++ src/screens/providerScreens/ProvServicesScreen/ProvServicesScreen.js
@@ -1,3 +1,4 @@
+
 import * as React from "react";
 import { Text, View, ImageBackground, TouchableOpacity } from "react-native";
 import Button from "./../../../../components/Button";
