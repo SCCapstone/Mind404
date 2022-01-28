8b1e5da66dedd2fae851a28909fc3bb3d4ebac83
Douglas Wendel <dpwendel@email.sc.edu>
2022-01-28 15:22:22 -0500

    more testing updates

--- __tests__/LogIn.test.js
+++ __tests__/LogIn.test.js
@@ -1,18 +1,26 @@
 import React from "react";
 import {render, fireEvent} from "@testing-library/react-native";
+import renderer from "react-test-renderer";
 import LoginScreen from "../src/screens/LoginScreen/LoginScreen";
 import { it } from "jest-circus";
+describe('renders default elements', () => {
 it("renders default elements", () => {
-    const { getAllByText, getByPlaceholderText } = render(<Login />);
+    const a = { getAllByText, getByPlaceholderText } = render(<Login />);
     getByPlaceholderText("E-mail");
     getByPlaceholderText("Password");
-});
+    expect(a).toMatchSnapshot();
+})
 
+});
+describe('shows the email address is badly formatted', () => {
 it('shows the email address is badly formatted', () => {
-    const { getByTetID,getByText } = render(<Login />);
+    const b = { getByTetID,getByText } = render(<Login />);
     fireEvent.press(getByTestId("Login.testButton"));
     getByText("Error: The email address is badly formatted.")
+    expect(b).toMatchSnapshot();
+})
 });
+
 jest.mock('react-native-keyboard-aware-scroll-view', () => {
         const KeyboardAwareScrollView = ({ children }) => children;
         return { KeyboardAwareScrollView };
