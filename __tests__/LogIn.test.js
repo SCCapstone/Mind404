049adab90d0e814c284bc5ff9ff2ac7779e424f4
Adam Farley <adamfarley4@gmail.com>
2022-01-30 17:00:39 -0500

    testing changes

--- __tests__/LogIn.test.js
+++ __tests__/LogIn.test.js
@@ -1,28 +1,47 @@
-import React from "react";
-import {render, fireEvent} from "@testing-library/react-native";
-import renderer from "react-test-renderer";
-import LoginScreen from "../src/screens/LoginScreen/LoginScreen";
-import { it } from "jest-circus";
+import * as React from "react";
+import HomeScreen from "../src/screens/clientScreens/HomeScreen/HomeScreen";
+import {render} from "@testing-library/react-native"
+
+it("renders",() => {
+    render(<HomeScreen/>);
+});
+/*
 describe('renders default elements', () => {
 it("renders default elements", () => {
-    const a = { getAllByText, getByPlaceholderText } = render(<Login />);
+    const a = { getAllByText, getByPlaceholderText } = render(<LoginScreen />);
     getByPlaceholderText("E-mail");
     getByPlaceholderText("Password");
     expect(a).toMatchSnapshot();
 })
 
 });
-describe('shows the email address is badly formatted', () => {
+
+
+ describe('shows the email address is badly formatted', () => {
 it('shows the email address is badly formatted', () => {
-    const b = { getByTetID,getByText } = render(<Login />);
+    const b = { getByTestID,getByText } = render(<LoginScreen />);
     fireEvent.press(getByTestId("Login.testButton"));
     getByText("Error: The email address is badly formatted.")
     expect(b).toMatchSnapshot();
 })
 });
 
+*/
 jest.mock('react-native-keyboard-aware-scroll-view', () => {
         const KeyboardAwareScrollView = ({ children }) => children;
         return { KeyboardAwareScrollView };
+
+});
+jest.mock('expo-status-bar', () => {
+    const expostatusbar = ({ children }) => children;
+    return { expostatusbar };
+});
+jest.mock('react-native-maps', () => {
+    const Mapview = ({ children }) => children;
+    return { Mapview };
+});
+jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => {
+    const MaterialCommunityIcons = ({ children }) => children;
+    return { MaterialCommunityIcons };
 });
 
