7db61739d5e758a57510590e73fe3314b4491fd7
Adam Farley <adamfarley4@gmail.com>
2022-02-20 13:45:30 -0500

    adding favservices screen

--- src/screens/clientScreens/FavServicesScreen/FavServicesScreen.js
+++ src/screens/clientScreens/FavServicesScreen/FavServicesScreen.js
@@ -1,16 +1,32 @@
-import * as React from "react";
-import { Text, View, ImageBackground } from "react-native";
-import styles from "../../../../components/styles";
+import { StatusBar } from "expo-status-bar";
+import React, { useContext, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity
+} from "react-native";
+import styles from "./../../../../components/styles";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { firebase } from "../../../firebase/config";
+import useUser from "../../../../useUser";
+import DateTimePicker from '@react-native-community/datetimepicker'
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-export default function ProvCalendarScreen() {
+export default function ClientFavoritesScreen({ route, navigation }) {
+ 
   return (
     <ImageBackground
       source={require("../../../../assets/GrubberBackground.png")}
       resizeMode="cover"
       style={styles.backgroundImage}
     >
-      <View>
-        <Text>Calendar</Text>
+      <View style={styles.container}>
+        <Text>ClientFavoritesScreen</Text>
       </View>
     </ImageBackground>
   );
