5c001605bcbaf94eb99c501862e0b7e83be2730e
PetrosYiannikouros <yianniko@email.sc.edu>
2022-04-06 17:39:13 -0400

    close #58

--- src/screens/providerScreens/ProvProfileScreen/ProvProfileScreen.js
+++ src/screens/providerScreens/ProvProfileScreen/ProvProfileScreen.js
@@ -5,7 +5,7 @@ import {
   ImageBackground,
   Image,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
 } from "react-native";
 import styles from "./../../../../components/styles";
 import useUser from "../../../../useUser";
@@ -28,80 +28,100 @@ export default function ProvProfileScreen({ navigation }) {
       .onSnapshot((querySnapshot) => {
         setProviderData(querySnapshot.data());
       });
-  }
+  };
 
   useEffect(() => {
-    getReviews()
+    getReviews();
   }, []);
 
   return (
     <ImageBackground
-    source={require("../../../../assets/GrubberBackground.png")}
-    resizeMode="cover"
+      source={require("../../../../assets/GrubberBackground.png")}
+      resizeMode="cover"
       style={styles.backgroundImage}
     >
       <ScrollView style={{ flex: 1, width: "100%", height: "100%" }}>
-      <TouchableOpacity
-        style={styles.settingsButton}
-        onPress={() => onPencilPress()}
-      >
-        <FontAwesome name="pencil" color="#788eec" size={30} />
-      </TouchableOpacity>
-      <View style={styles.profileLayout}>
-        <Text style={styles.welcome}>
-          {user.firstName} {user.lastName}
-        </Text>
-        <Image
-          style={styles.profilePhoto}
-          source={
-            user.imageURL
-              ? { uri: user.imageURL }
-              : require("../../../../assets/unknown-user-image.png")
-          }
-        />
-        <View style={styles.profileDescriptionWrapper}>
-          <View>
-            <Text style={styles.titleText}>Service Provided</Text>
+        <TouchableOpacity
+          style={styles.settingsButton}
+          onPress={() => onPencilPress()}
+        >
+          <FontAwesome name="pencil" color="#788eec" size={30} />
+        </TouchableOpacity>
+        <View style={styles.profileLayout}>
+          <Text style={styles.welcome}>
+            {user.firstName} {user.lastName}
+          </Text>
+          <Image
+            style={styles.profilePhoto}
+            source={
+              user.imageURL
+                ? { uri: user.imageURL }
+                : require("../../../../assets/unknown-user-image.png")
+            }
+          />
+          <View style={styles.profileDescriptionWrapper}>
+            <View>
+              <Text style={styles.titleText}>Service Provided</Text>
+            </View>
+            <View style={styles.marginTop10}>
+              <Text>{user.description}</Text>
+            </View>
           </View>
-          <View style={styles.marginTop10}>
-            <Text>{user.description}</Text>
+          <View style={styles.profileDescriptionWrapper}>
+            <View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{ fontSize: 20, fontWeight: "bold", color: "black" }}
+                >
+                  Your Rating:{" "}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 24,
+                    fontWeight: "bold",
+                    color: "green",
+                    fontStyle: "italic",
+                  }}
+                >
+                  {providerData.avgRating}
+                </Text>
+              </View>
+              <Text style={styles.titleText}>Reviews</Text>
+            </View>
+            <View style={styles.marginTop10}>
+              {providerData.reviews && providerData.reviews.length == 0 && (
+                <Text>This Service Provider has no reviews yet.</Text>
+              )}
+              {providerData.reviews &&
+                providerData.reviews.length > 0 &&
+                providerData.reviews.map((item, index) => {
+                  return (
+                    <View
+                      style={{
+                        borderWidth: 1,
+                        borderStyle: "solid",
+                        borderColor: "#000",
+                        padding: 10,
+                        borderRadius: 5,
+                        marginBottom: 10,
+                      }}
+                      key={index}
+                    >
+                      <Text>{`${item.firstName} ${item.lastName}`}</Text>
+                      <Text>{item.description}</Text>
+                      <Text>{item.rating}/5.0</Text>
+                    </View>
+                  );
+                })}
+            </View>
           </View>
         </View>
-        <View style={styles.profileDescriptionWrapper}>
-          <View>
-            <View style={{flexDirection: 'row', alignContent: 'center', alignItems: "center"}}>
-              <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>Your Rating: </Text>
-              <Text style={{fontSize: 24, fontWeight: "bold", color: "green", fontStyle: 'italic', }}>{providerData.avgRating}</Text>
-            </View>
-            <Text style={styles.titleText}>Reviews</Text>
-          </View>
-          <View style={styles.marginTop10}>
-            {providerData.reviews && providerData.reviews.length == 0 && (
-              <Text>This Service Provider has no reviews yet.</Text>
-            )}
-            {providerData.reviews &&
-              providerData.reviews.length > 0 &&
-              providerData.reviews.map((item) => {
-                return (
-                  <View
-                    style={{
-                      borderWidth: 1,
-                      borderStyle: "solid",
-                      borderColor: "#000",
-                      padding: 10,
-                      borderRadius: 5,
-                      marginBottom: 10,
-                    }}
-                  >
-                    <Text>{`${item.firstName} ${item.lastName}`}</Text>
-                    <Text>{item.description}</Text>
-                    <Text>{item.rating}/5.0</Text>
-                  </View>
-                );
-              })}
-              </View>
-          </View>
-      </View>
       </ScrollView>
     </ImageBackground>
   );
