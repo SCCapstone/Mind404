028f5f5b585be6fb1cda91e2eebdf21ac0f1b937
Adam Farley <adamfarley4@gmail.com>
2022-04-13 15:20:17 -0400

    addevent test layout

---
+++ __tests__/AddEvent.test.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "../components/Button";
+import renderer from "react-test-renderer";
+import {Flatlist,Text} from 'react-native'
+import{fireEvent} from "@testing-library/react-native"
+import AddEvent from "../src/screens/ProviderScreens/ProvCalendarScreen/AddEvent"
+
+describe('AddEvent', () => {
+    it('shows added event correctly', () =>{
+
+
+    });
+});
