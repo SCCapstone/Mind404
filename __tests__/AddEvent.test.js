e2c9e412d136e5738118170fa42cee6437bb6d15
Adam Farley <adamfarley4@gmail.com>
2022-04-19 13:34:12 -0400

    more changes to tests

--- __tests__/AddEvent.test.js
+++ __tests__/AddEvent.test.js
@@ -9,8 +9,7 @@ import { render, fireEvent } from '@testing-library/react-native';
 test('fire changeText event', () => {
   const onEventMock = jest.fn();
   const { getByPlaceholderText } = render(
-    // MyComponent renders TextInput which has a placeholder 'Enter details'
-    // and with `onChangeText` bound to handleChangeText
+    // MyComponent renders TextInput
     <MyComponent handleChangeText={onEventMock} />
   );
 
--- __tests__/ClientFavServicesScreen.test.js
+++ __tests__/ClientFavServicesScreen.test.js
@@ -4,3 +4,4 @@ import renderer from "react-test-renderer";
 import {Flatlist,Text} from 'react-native'
 import{fireEvent} from "@testing-library/react-native"
 import FavServicesScreen from "../src/screens/clientScreens/FavServicesScreen"
+
